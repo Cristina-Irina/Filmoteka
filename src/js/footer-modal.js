3df2c5196f90ef9a18ca6c99905578c1d6b1419b
Mihail <Maria@MihailMichael>
2023-07-23 12:31:21 +0300

    feat: FooterModal New

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,1 +1,15 @@
 // FT-23 Creează o fereastră modală (mobile, tablet, desktop) cu informații despre membrii echipei tale, care se va deschide la click pe textul "GoIT Students" din footer.
+const student = document.querySelector('.footer-modal_students');
+const dropModal = document.querySelector('.modal');
+const modal = document.querySelector('.modal-content');
+function openModal() {
+  dropModal.style.display = 'block';
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  dropModal.style.display = 'none';
+  modal.style.display = 'none';
+}
+student.onclick = openModal;
+dropModal.onclick = closeModal;
