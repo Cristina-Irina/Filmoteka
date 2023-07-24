0cc545ed4044b37697e97ba8ddc3f6eb365fd494
Cristina-Irina <121606106+Cristina-Irina@users.noreply.github.com>
2023-07-24 18:54:23 +0000

    Deploying to gh-pages from @ Cristina-Irina/JS-project@111ed22f3bcca0e4a9056fd98919e26e2a49f771 🚀

--- index.eee4fdc2.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".footer-modal_students"),o=document.querySelector(".modal"),l=document.querySelector(".modal-content");e.onclick=function(){o.style.display="block",l.style.display="block"},o.onclick=function(){o.style.display="none",l.style.display="none"}}();
-//# sourceMappingURL=index.eee4fdc2.js.map
