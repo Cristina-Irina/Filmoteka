1f41601a77ef579363b17bd8f71675c2bf3c2fc6
Cristina-Irina <cristina.rmackay@gmail.com>
2023-07-30 19:45:33 +0300

    empty-library

---
+++ src/js/emptyLibrary.js
@@ -0,0 +1,18 @@
+const noneCardList = document.querySelector('.gallery');
+const blockEmptyLib = document.querySelector('.library-gallery');
+
+export function emptyLibrary() {
+  noneCardList.innerHTML = `
+  <li class="library-gallery">
+    <div class="container library-gallery__wrap">
+      <img class="library-gallery__image" src="./images/library-img/film2.png" alt="" width="750"/>
+      <p class="library-gallery__text">The library is empty.</p>
+      <p class="library-gallery__text">Add some movies.</p>
+    </div>
+  </li>
+`;
+  try {
+    noneCardList.innerHTML = '';
+    blockEmptyLib.classList.add('active');
+  } catch (err) {}
+}
